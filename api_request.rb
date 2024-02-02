1f7a79c1971ca1e69b918b500d1de01608e6c0aa
Partha Paul <pparthapaul11@gmail.com>
2024-02-02 17:30:47 +0530

    next apis

--- api_request.rb
+++ api_request.rb
@@ -19,7 +19,54 @@ request_body = {
 }
 
 # example
+
 api_client = APIClient.new('https://api.example.com')
+
+# offer exsistance api 
 response = api_client.post('/endpoint',request_body)
+puts "Response Code: #{response.code}"
+puts "Response Body: #{response.body}"
+
+#offer details api (for existing offer )
+offer_details ={
+ "requestId": "34e75862-32dc-4b16-aa4a-410446865404",
+ "requestDatetime": "2019-11-26T12:28:56.321+05:30",
+ "partnerCode": "PAR01",
+ "applicationReferenceNumber": "Partner App Reference like Lead Id",
+ "uid": "unique Id from the partner",
+ "otp": "123456",
+ "accountNumber": "058374029348023",
+ "customerType": "ETB|NTB",
+ "etbType": "WO|NO",
+ "demogDetails": {
+ "entityDetails": {
+ "name": "Bradley Gomez",
+ "pan": "ubifao",
+ "doi": "merwufci",
+ "address": "1109 Cerkes Court",
+ "email": "zukgehza@buom.km",
+ "state": "RI",
+ "city": "Butijisim",
+ "pincode": "ojucem",
+ "constitution": "misok",
+ "mobileNumber": "6188922889568256"
+ },
+ "promoterDetails": [
+ {
+ "fullName": "Benelli",
+ "gender": "Female",
+ "mobileNumber": "3873162085269504",
+ "dateOfBirth": "2/8/1973",
+ "pan": "ezezamuwele",
+ "address": "649 Hupod Pike",
+ "city": "Vaplostew",
+ "state": "FL",
+ "pincode": "ruhadataguckon",
+ "shareholdingPattern": "husuhealai"
+ }
+ ]
+ }
+}
+response = api_client.get('/endpoint', offer_details)
 puts "Response Code: #{response.code}"
 puts "Response Body: #{response.body}"
